88cec217bc93a4150bb51ba3364d1c17b1b51c52
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-03-03 23:54:59 -0600

    Update bunnies.py

--- bunnies.py
+++ bunnies.py
@@ -166,20 +166,19 @@ from math import *
 from random import *
 import time
 
+
 def main():
     win = GraphWin("Bunny", 1000, 1000)
     win.setBackground("light cyan")
     win.setCoords(0, 0, 1000, 1000)
-    ground = Line(Point(0, 10), Point(1000, 10))
-    ground.setOutline("lightgreen")
-    ground.setWidth(20)
-    ground.draw(win)
+    ground = Ground()
+    ground.ground.draw(win)
     xposition = [0, 1000/3, 2000/3, 1000]
     xp = choice(xposition)
     grass = Grass(xp)
     grass.grass.draw(win)
     
-    #bunny -- all starting at x = 0 for now
+    #bunny 
     x = randrange(0, 1000)
     y = 10 * abs(40 * sin((3 * pi/1000) * x))
     bun = Bunny(x, y)
@@ -193,7 +192,7 @@ def main():
 
     #eagle
     c = randrange(0, 1000)
-    d = 400 * sin((1/90)* c + 18) + 500
+    d = 400 * sin((1/90)* c + 18) + 505
     eagle = Eagle(c, d)
     eagle.eagle.draw(win)
     
@@ -211,8 +210,8 @@ def main():
         x += 3
         dy = (18 * pi * cos((3*pi*x)/1000) *sin((3*pi*x)/1000))/(5 * abs(sin((3*pi*x)/1000)))
         bun.bun.move(3, dy)
-        #bun.bun_move_right()
-        a += 0.01
+        #bun.bun_move_right() -- tyring to figure this out
+        a += 0.5
         db = 0
         fox.fox.move(a,db)
         c += 3
@@ -220,6 +219,12 @@ def main():
         eagle.eagle.move(5, dd)
 
 #how to make grass and bunny simultanous
+class Ground:
+    def __init__(self):
+        self.ground = Line(Point(0, 10), Point(1000, 10))
+        self.ground.setOutline("lightgreen")
+        self.ground.setWidth(20)
+
 class Grass:
    def __init__(self, xp):
        self.xp = xp
@@ -230,28 +235,25 @@ class Grass:
 
 class Bunny:
    def __init__(self, x, y):
+       #x = randrange(0, 1000)
+       #y = 10 * abs(40 * sin((3 * pi/1000) * x))
        self.x = x
        self.y = y
        self.bun = Circle(Point(self.x, self.y), 50)
        self.bun.setFill("pink")
 
-   def bun_move_right(self):
+   def bun_move_right(self, dy):
        dx = 3
        self.dx = dx
        self.dx += 3
-       self.dy = (18 * pi * cos((3* pi * self.dx)/1000) * sin((3 * pi * self.dx)/1000))/(5 * abs(sin((3 * pi * self.dx)/1000)))
-       self.bun.move(self.dx, self.dy)
-
-   def bun_move_left(self,dy):
-       dx = 3
-       self.dx = dx
-       self.dx -= 3
        self.dy = dy
        self.dy = (18 * pi * cos((3* pi * self.dx)/1000) * sin((3 * pi * self.dx)/1000))/(5 * abs(sin((3 * pi * self.dx)/1000)))
        self.bun.move(self.dx, self.dy)
 
 class Foxy:
    def __init__(self, a, b):
+       #x = randrange(0, 500)
+       #y = 100
        self.a = a
        self.b = b
        self.fox = Circle(Point(self.a, self.b), 50)
@@ -259,10 +261,13 @@ class Foxy:
 
 class Eagle:
    def __init__(self, c, d):
+       #x = randrange(0, 1000)
+       #y = 400 * sin((1/900)*self.x + 18) + 500
        self.c = c
        self.d = d
        self.eagle = Circle(Point(self.c, self.d), 50)
        self.eagle.setFill("peru")
+
 
 main()
 '''
