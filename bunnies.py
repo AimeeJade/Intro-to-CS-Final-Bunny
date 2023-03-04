7646774c52fa69efa49592993c3df1443f2ecd7e
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-03-04 14:51:55 -0600

    Update bunnies.py

--- bunnies.py
+++ bunnies.py
@@ -2,145 +2,160 @@ from graphics import *
 from math import *
 from random import *
 import time
+
+
 def main():
+
+    #creating a window and the background
     win = GraphWin("Bunny", 1000, 1000)
     win.setBackground("light cyan")
     win.setCoords(0, 0, 1000, 1000)
     ground = Ground()
     ground.ground.draw(win)
-    xposition = [0, 1000/3, 2000/3, 1000]
-    xp = choice(xposition)
-    grass = Grass(xp)
+    grass_choice = [0, 1000/3, 2000/3, 1000]
+    grass_x = choice(grass_choice)
+    grass = Grass(grass_x)
     grass.grass.draw(win)
-    
+
+#------------------------------------------------------
     #bunny 
-    x = randrange(0, 1000)
-    y = 10 * abs(40 * sin((3 * pi/1000) * x)) + 40
-    bun = Bunny(x, y)
+    bun_x = randrange(0, 1000)
+    bun_y = 10 * abs(40 * sin((3 * pi/1000) * bun_x)) + 40
+    bun = Bunny(bun_x, bun_y)
     bun.bun.draw(win)
+    
     #fox
-    a = randrange(0, 1000)
-    b = 100
-    fox = Foxy(a, b)
+    fox_x = randrange(0, 1000)
+    fox_y = 100
+    fox = Foxy(fox_x, fox_y)
     fox.fox.draw(win)
+   
     #eagle
-    c = randrange(0, 1000)
-    d = 400 * sin((1/90)* c + 18) + 350
-    eagle = Eagle(c, d)
+    eagle_x = randrange(0, 1000)
+    eagle_y = 400 * sin((1/90)* eagle_x + 18) + 350
+    eagle = Eagle(eagle_x, eagle_y)
     eagle.eagle.draw(win)
+#------------------------------------------------------
 
     right = True
     right2 = True
     right3 = True
-    #main while loop
+    
     while win.checkKey() != 'q': 
-        dy = (18 * pi * cos((3*pi*x)/1000) *sin((3*pi*x)/1000))/(5 * abs(sin((3*pi*x)/1000)))
-        dd = (140 * cos((c/90) + 18))/9
+        bun_slope = (18 * pi * cos((3 * pi * bun_x) / 1000) * sin((3 * pi * bun_x) / 1000))/(5 * abs(sin((3 * pi * bun_x) / 1000)))
+        eagle_slope = (140 * cos((eagle_x/90) + 18))/9
         
-        if bun.bun.getCenter().getX() >= xp - 5 and bun.bun.getCenter().getX() <= xp + 5:
+        #eating interation
+        if bun.bun.getCenter().getX() >= grass_x - 5 and bun.bun.getCenter().getX() <= grass_x + 5:
             grass.grass.undraw()
-            xp = choice(xposition)
-            grass = Grass(xp)
+            grass_x = choice(grass_choice)
+            grass = Grass(grass_x)
             grass.grass.draw(win)
-        if fox.fox.getCenter().getY() >= y - 10 and fox.fox.getCenter().getY() <= y + 10:
+
+        if (fox.fox.getCenter().getY() >= bun_y - 50 and fox.fox.getCenter().getY() <= bun_y + 50) and (fox.fox.getCenter().getX() >= bun_x - 50 and fox.fox.getCenter().getX() <= bun_x + 50):
             bun.bun.undraw()
-        if eagle.eagle.getCenter().getY() >= b - 10 and eagle.eagle.getCenter().getY() <= b + 10:
+
+        if (eagle.eagle.getCenter().getY() >= fox_y - 10 and eagle.eagle.getCenter().getY() <= fox_y + 10) and (eagle.eagle.getCenter().getX() >= fox_x - 10 and eagle.eagle.getCenter().getX() <= fox_x + 10):
             fox.fox.undraw()
 
-    #circle 1 movement
+    
         time.sleep(0.01)
 
-        
-        if x > 1000: #checking to change direction if the circle is out of bounds on either end
-            x -= 3
-            bun.bun.move(-3, -dy)
+        #bunny movement
+        if bun_x > 1000: 
+            bun_x -= 3
+            bun.bun.move(-3, -bun_slope)
             right = False
-        elif x < 0:
-            x += 3
-            bun.bun.move(3, dy)
+        elif bun_x < 0:
+            bun_x += 3
+            bun.bun.move(3, bun_slope)
             right = True
         if right: #movement based on the direction given by "right" variable
-            x += 3
-            bun.bun.move(3, dy)
+            bun_x += 3
+            bun.bun.move(3, bun_slope)
         else:
-            x -= 3
-            bun.bun.move(-3,-dy)
+            bun_x -= 3
+            bun.bun.move(-3,-bun_slope)
             
-    #fox movement
-    
-        if a > 1000:
-            a -= 10
+        #fox movement
+        if fox_x > 1000:
+            fox_x -= 10
             fox.fox.move(-10,0)
             right2 = False
-        elif a < 0:
-            a += 10
+        elif fox_x < 0:
+            fox_x += 10
             fox.fox.move(10,0)
             right2 = True
         if right2:
-            a += 10
+            fox_x += 10
             fox.fox.move(10,0)
         else:
-            a -= 10
+            fox_x -= 10
             fox.fox.move(-10,0)
 
-        #eagle
-        if c > 1000:
-            c -= 5
-            eagle.eagle.move(-5,-dd)
+        #eagle movement
+        if eagle_x > 1000:
+            eagle_x -= 5
+            eagle.eagle.move(-5,-eagle_slope)
             right3 = False
-        elif c < 0:
-            c += 5
-            eagle.eagle.move(5,dd)
+        elif eagle_x < 0:
+            eagle_x += 5
+            eagle.eagle.move(5,eagle_slope)
             right3 = True
         if right3:
-            c += 5
-            eagle.eagle.move(5,dd)
+            eagle_x += 5
+            eagle.eagle.move(5,eagle_slope)
         else:
-            c -= 5
-            eagle.eagle.move(-5,-dd)
+            eagle_x -= 5
+            eagle.eagle.move(-5,-eagle_slope)
     
-
+#classes
 class Ground:
     def __init__(self):
         self.ground = Line(Point(0, 30), Point(1000, 30))
         self.ground.setOutline("lightgreen")
         self.ground.setWidth(60)
+
 class Grass:
-   def __init__(self, xp):
-       self.xp = xp
+   def __init__(self, grass_x):
+       self.grass_x = grass_x
        self.y = 0
-       self.grass = Circle(Point(xp, self.y), 50)
+       self.grass = Circle(Point(grass_x, self.y), 50)
        self.grass.setFill("lightgreen")
     
 class Bunny:
-   def __init__(self, x, y):
+   def __init__(self, bun_x, bun_y):
        #x = randrange(0, 1000)
        #y = 10 * abs(40 * sin((3 * pi/1000) * x))
-       self.x = x
-       self.y = y
+       self.x = bun_x
+       self.y = bun_y
        self.bun = Circle(Point(self.x, self.y), 50)
        self.bun.setFill("pink")
+   
    def bun_move_right(self, dy):
-       dx = 3
-       self.dx = dx
-       self.dx += 3
+       self.x -= 3
        self.dy = dy
-       self.dy = (18 * pi * cos((3* pi * self.dx)/1000) * sin((3 * pi * self.dx)/1000))/(5 * abs(sin((3 * pi * self.dx)/1000)))
-       self.bun.move(self.dx, self.dy)
+       self.bun.move(-3, -self.dy)
+
+
 class Foxy:
-   def __init__(self, a, b):
+   def __init__(self, fox_x, fox_y):
        #x = randrange(0, 500)
        #y = 100
-       self.a = a
-       self.b = b
-       self.fox = Circle(Point(self.a, self.b), 50)
+       self.x = fox_x
+       self.y = fox_y
+       self.fox = Circle(Point(self.x, self.y), 50)
        self.fox.setFill("coral")
+
+
 class Eagle:
-   def __init__(self, c, d):
+   def __init__(self, eagle_x, eagle_y):
        #x = randrange(0, 1000)
        #y = 400 * sin((1/900)*self.x + 18) + 500
-       self.c = c
-       self.d = d
-       self.eagle = Circle(Point(self.c, self.d), 50)
+       self.x = eagle_x
+       self.y = eagle_y
+       self.eagle = Circle(Point(self.x, self.y), 50)
        self.eagle.setFill("peru")
+
+
 main()
