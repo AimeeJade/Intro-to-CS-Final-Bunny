caca3836b8f7e9856ddc8e02648dbffc9e01de9f
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-03-04 14:52:15 -0600

    Update bunnies.py

--- bunnies.py
+++ bunnies.py
@@ -2,6 +2,29 @@ from graphics import *
 from math import *
 from random import *
 import time
+from tkinter import *
+from PIL import Image
+import numpy as np
+
+im = Image.open('TBunny.png')
+immat = im.load()
+(X, Y) = im.size
+m = np.zeros((X, Y))
+
+for x in range(X):
+    for y in range(Y):
+        m[x, y] = immat[(x, y)] != (255, 255, 255)
+m = m / np.sum(np.sum(m))
+
+# marginal distributions
+dx = np.sum(m, 1)
+dy = np.sum(m, 0)
+
+# expected values
+cx = np.sum(dx * np.arange(X))
+cy = np.sum(dy * np.arange(Y))
+
+print(cx, cy)
 
 
 def main():
