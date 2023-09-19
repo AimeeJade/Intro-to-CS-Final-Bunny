ca28dbcdc1efa7784c32d44835d8ddbe92383f44
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-09-18 20:07:37 -0500

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -17,57 +17,58 @@ public class Welcome {
         
         //asking for an integer
         System.out.println("Please enter an integer");
+        try {
+            int number = scan.nextInt();
+            int numValue = Integer.signum(number); //checking if the integer inputed is postive, negative, or 0
+
+            //Printing user's name
+            System.out.println("Welcome " + username);
+
+            //Printing name backwards using a for loop over the length of the name
+            int lengthOfUsername = username.length();
+
+            StringBuilder nameBackwards = new StringBuilder();
+            for (int i = 1; i <= lengthOfUsername; i++){
+                nameBackwards.append(username.charAt(lengthOfUsername - i));
+            }
+
+            System.out.println("Your name backwards is " + nameBackwards);
         
-        
-        int number = scan.nextInt();
-        int numValue = Integer.signum(number); //checking if the integer inputed is postive, negative, or 0
-
-        //Printing user's name
-        System.out.println("Welcome " + username);
-
-        //Printing name backwards using a for loop over the length of the name
-        int lengthOfUsername = username.length();
-
-        StringBuilder nameBackwards = new StringBuilder();
-        for (int i = 1; i <= lengthOfUsername; i++){
-            nameBackwards.append(username.charAt(lengthOfUsername - i));
-        }
-
-        System.out.println("Your name backwards is " + nameBackwards);
-        
-        //Printing out a triangle made of integers
-        if (numValue == 1){ //If the integer the user inputed is positive
-            for (int j = 1; j <= number; j++){
-                for (int k = 1; k <= j; k++){
-                    if (k < j){
-                        System.out.print(j + ", ");
-                }
-                else {
-                    System.out.println(j);
+            //Printing out a triangle made of integers
+            if (numValue == 1){ //If the integer the user inputed is positive
+                for (int j = 1; j <= number; j++){
+                    for (int k = 1; k <= j; k++){
+                        if (k < j){
+                            System.out.print(j + ", ");
+                    }
+                    else {
+                        System.out.println(j);
+                    }
                 }
             }
-        }
 
-        }
-
-        if (numValue == -1){ //If the integer the user inputed was negative
-            for (int j = -1; j >= number; j--){
-                for (int k = -1; k >= j; k--){
-                    if (k > j){
-                        System.out.print(j + ", ");
-                }
-                else {
-                    System.out.println(j);
-                }
             }
-    }
 
+            if (numValue == -1){ //If the integer the user inputed was negative
+                for (int j = -1; j >= number; j--){
+                    for (int k = -1; k >= j; k--){
+                        if (k > j){
+                            System.out.print(j + ", ");
+                    }
+                    else {
+                        System.out.println(j);
+                    }
+                }
         }
-        if (numValue == 0){
-            System.out.println("Cannot print a triangle of height 0");
-        }
-        
-        
+
+            }
+            if (numValue == 0){
+                System.out.println("Cannot print a triangle of height 0");
+            }
+            }
+            catch(Exception e) {
+                System.out.println("You must enter an integer");
+            }
 
     } 
 }
