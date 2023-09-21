254fc5e9523720810e96a75d5609132478ddb2ab
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-09-21 18:24:42 -0500

    Create MazeSquare.java

---
+++ MazeSquare.java
@@ -0,0 +1,24 @@
+/**
+ * MazeSquare.java
+ * A helper class for maze solving assignment.
+ * Represents a single square within a rectangular maze.
+ *
+ * @author ANIKA RAJBHANDRY and AIMEE YUAN
+ */
+public class MazeSquare {
+    String height;
+    String length;
+
+    // A constructor, taking whatever parameters you decide:
+    public MazeSquare(String height, String length) {
+        for (i = 0; i <= height; i++){
+            
+        }
+    }
+
+    // Whatever methods you want, such as:
+    public boolean hasTopWall() {
+        // YOUR CODE (and new return statement) HERE
+        return false;
+    }
+}
