e9bdd42d1ed52e0b39bd080852d9eea31f2dd5bb
AimeeJade <126742356+AimeeJade@users.noreply.github.com>
2023-09-24 20:12:54 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -2,30 +2,55 @@
  * Maze.java
  * A class for loading and printing mazes from files.
  *
- * @author ANIKA RAJBHANDARY and AIMEE YUAN
+ * @author YOUR NAME AND YOUR PARTNER'S NAME
  */
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.io.FileNotFoundException;
-import java.io.File;
+import java.util.*;
+import java.io.*;
+
 
 public class Maze {
     private ArrayList<ArrayList<MazeSquare>> rowList;
     private int w;
     private int h;
-    private int start_x;
-    private int start_y;
-
+    private int s1;
+    private int s2;
+    private int f1;
+    private int f2;
     // OTHER INSTANCE VARIABLES IF YOU NEED THEM
     
-    
-    //Constructor for the Maze class
-    public Maze() {
-        rowList = new ArrayList<ArrayList<MazeSquare>>();
+    /**
+     * Constructor for the Maze class
+     */
+    //public Maze(){
+        //rowList = new ArrayList<ArrayList<MazeSquare>>();
+        //creating an array of dimension w and dimention
+        // MazeSquare[w][h];
+
     }
-    
-    //Load in a Maze from a given file
-    String fileName = "maze.txt";
+
+    public void makeEmptyGrid(int witdh, int height){
+        rowList = new ArrayList<ArrayList<MazeSquare>>();
+
+        int w = 0;
+        int h = 0;
+        while size(r < size){
+            ArrayList<MazeSquare> row = new ArrayLis<MazeSquare>();
+            for (int c = 0; c < size; c++){
+                row.add(new MazeSquare());
+            }
+            grid.add(row);
+        }
+        
+
+    }
+
+
+
+    /**
+     * Load in a Maze from a given file
+     *
+     * @param fileName the name of the file containing the maze
+     */
     public void load(String fileName) {
         // Create a scanner for the given file
         Scanner scanner = null;
@@ -41,36 +66,54 @@ public class Maze {
         w = Integer.parseInt(lineParams[0]);
         h = Integer.parseInt(lineParams[1]);
         
-        // YOUR CODE TO FINISH LOADING FILE HERE
-        String[] startBlock = scanner.nextLine().split(" ");
-        start_x = Integer.parseInt(startBlock[0]);
-        start_y = Integer.parseInt(startBlock[1]);
+        lineParams = scanner.nextLine().split(" ");
+
+        s1 = Integer.parseInt(lineParams[0]);
+        s2 = Integer.parseInt(lineParams[1]);
+        
+        lineParams = scanner.nextLine().split(" ");
+
+        f1 = Integer.parseInt(lineParams[0]);
+        f2 = Integer.parseInt(lineParams[1]);
+
+        ArrayList<ArrayList<String>> inputList = new ArrayList<>();
+        for(int i = 1; i<=h; i++){
+            lineParams = scanner.nextLine().split("");
+            ArrayList<String> strList = new ArrayList<String>(Arrays.asList(lineParams));
+            inputList.add(strList);
+        }
+
     }
+
+
 
     /**
      * Print the Maze to System.out
      */
     public void print() {
-        // YOUR CODE HERE
+        
     }
-
-    // MORE METHODS AS YOU NEED THEM
-
-public int calc_size(){
-    return w * h;
-}
+    //add the right wall here
 
 
     // This main program acts as a simple unit test for the
     // load-from-file and print-to-System.out Maze capabilities.
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java Maze mazeFile");
-            System.exit(1);
-        }
+        // if (args.length != 1) {
+        //     System.err.println("Usage: java Maze mazeFile");
+        //     System.exit(1);
+        // }
 
+        //for (x < = w);
         Maze maze = new Maze();
-        maze.load(args[0]);
+        // MazeSquare maze1 = new MazeSquare();
+        // maze1.get____
+        // maze1. set blah 
+
+
+        // maze1.printShapeType();
+
+        maze.load("maze.txt");
         maze.print();
     }
 }
